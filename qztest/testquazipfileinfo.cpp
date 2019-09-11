7fc7b8093b815088eb4b49bd96eb81b2739ce8eb
Sergei Tachenov <sergei@tachenov.name>
2019-09-11 21:13:05 +0300

    Fixed commit #39 (PR #22 fix)
    toUTC() call was lost in that commit, causing tests to fail

--- qztest/testquazipfileinfo.cpp
+++ qztest/testquazipfileinfo.cpp
@@ -42,9 +42,9 @@ void TestQuaZipFileInfo::getNTFSTime()
         QDateTime lm = fileInfo.lastModified().toUTC();
         QDateTime lr = fileInfo.lastRead().toUTC();
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 10, 0))
-        QDateTime cr = fileInfo.birthTime();
+        QDateTime cr = fileInfo.birthTime().toUTC();
 #else
-        QDateTime cr = fileInfo.created();
+        QDateTime cr = fileInfo.created().toUTC();
 #endif
         mTicks = (static_cast<qint64>(base.date().daysTo(lm.date()))
                 * Q_UINT64_C(86400000)
