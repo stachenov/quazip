999518d369b87c23f9944c04c6b432f8ae5fa952
Sergei Tachenov <sergei@tachenov.name>
2022-07-02 19:40:54 +0300

    Move version defines into a generated header
    This way these defines will stay regardless of the way the user chooses
    to integrate QuaZip (even if they are not using CMake at all).

--- quazip/quazip_global.h
+++ quazip/quazip_global.h
@@ -27,7 +27,8 @@ see quazip/(un)zip.h files for details. Basically it's the zlib license.
 
 #include <QtCore/QtGlobal>
 
-#define QUAZIP_VERSION ((QUAZIP_VERSION_MAJOR << 24) | (QUAZIP_VERSION_MINOR << 16) | (QUAZIP_VERSION_PATCH << 8) | QUAZIP_VERSION_TWEAK)
+#include "quazip_version.h"
+
 
 /**
   This is automatically defined when building a static library, but when
