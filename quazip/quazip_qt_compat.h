f2ff7645397bec0dc9581d1858ac927d4bfe3ca8
geustache <geustache@intuisphere.com>
2024-06-23 11:54:33 +0200

    clean obsolete code

--- quazip/quazip_qt_compat.h
+++ quazip/quazip_qt_compat.h
@@ -12,14 +12,6 @@
 #include <QtCore/Qt>
 #include <QtCore/QtGlobal>
 
-// Legacy encodings are still everywhere, but the Qt team decided we
-// don't need them anymore and moved them out of Core in Qt 6.
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-//#  include <QtCore5Compat/QTextCodec>
-#else
-#  include <QtCore/QTextCodec>
-#endif
-
 #include "quazip_textcodec.h"
 
 // QSaveFile terribly breaks the is-a idiom (Liskov substitution principle):
