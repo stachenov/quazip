c290c4d1374ed342e5f175e8359be1820b0c83a9
cen1 <cen.is.imba@gmail.com>
2024-10-20 21:39:40 +0200

    draft for JlCompress utf8

--- quazip/JlCompress.cpp
+++ quazip/JlCompress.cpp
@@ -98,6 +98,8 @@ bool JlCompress::compressSubDir(QuaZip* zip, QString dir, QString origDir, bool 
         zip->getMode()!=QuaZip::mdAppend &&
         zip->getMode()!=QuaZip::mdAdd) return false;
 
+    zip->setUtf8Enabled(options.getUtf8Enabled());
+
     QDir directory(dir);
     if (!directory.exists()) return false;
 
@@ -230,6 +232,8 @@ bool JlCompress::compressFile(QString fileCompressed, QString file) {
 bool JlCompress::compressFile(QString fileCompressed, QString file, const Options& options) {
     // Create zip
     QuaZip zip(fileCompressed);
+    zip.setUtf8Enabled(options.getUtf8Enabled());
+
     QDir().mkpath(QFileInfo(fileCompressed).absolutePath());
     if(!zip.open(QuaZip::mdCreate)) {
         QFile::remove(fileCompressed);
@@ -259,6 +263,7 @@ bool JlCompress::compressFiles(QString fileCompressed, QStringList files) {
 bool JlCompress::compressFiles(QString fileCompressed, QStringList files, const Options& options) {
   // Create zip
   QuaZip zip(fileCompressed);
+  zip.setUtf8Enabled(options.getUtf8Enabled());
   QDir().mkpath(QFileInfo(fileCompressed).absolutePath());
   if(!zip.open(QuaZip::mdCreate)) {
     QFile::remove(fileCompressed);
@@ -301,6 +306,7 @@ bool JlCompress::compressDir(QString fileCompressed, QString dir,
 {
   // Create zip
   QuaZip zip(fileCompressed);
+  zip.setUtf8Enabled(options.getUtf8Enabled());
   QDir().mkpath(QFileInfo(fileCompressed).absolutePath());
   if(!zip.open(QuaZip::mdCreate)) {
     QFile::remove(fileCompressed);
